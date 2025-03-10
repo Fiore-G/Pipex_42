a5d1caeb491d368465f1396343beb18fcba67b47
Fiorella G <fleurelle.g@gmail.com>
2025-03-10 20:25:04 +0100

    Update pipex.c

--- src/pipex.c
+++ src/pipex.c
@@ -54,5 +54,30 @@ void  child_process_2(int pipe_fd[2], char *outfile, char *cmd2, char **env_vars
 
 int  main(int argc, char **argv, char **env_vars)
 {
-  
+  int  pipe_fd[2];
+  pid_t  pid1;
+  pid_t  pid2;
+
+  if (argc != 5)
+  {
+    write(2, "Usage: ./pipex infile cmd1 cmd2 outfile\n", 40);
+    exit(EXIT_FAILURE);
+  }
+  if (pipe(pipe_fd) == -1)
+    error();
+  pid1 = fork();
+  if (pid1 == -1)
+    error();
+  if (pid1 == 0)
+    child_process_1(pipe_fd, argv[1], argv[2], env_vars);
+  pid2 = fork();
+  if (pid2 == -1)
+    error();
+  if (pid2 == 0)
+    child_process_2(pipe_fd, argv[4], argv[3], env_vars);
+  close(pipe_fd[0]);
+  close(pipe_fd[1]);
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
+  return (0);
 }
