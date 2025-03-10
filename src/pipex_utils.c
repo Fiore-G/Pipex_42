456f23e89f56c102ca68fc06cc5bd9e3452a5adb
Fiorella G <fleurelle.g@gmail.com>
2025-03-10 13:06:20 +0100

    Update pipex_utils.c

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -1,5 +1,26 @@
 #include "pipex.h"
 
+void  execute_command(char *cmd, char **env_vars)
+{
+  char **args;
+
+  args = ft_split(cmd, ' ');
+  if (!args)
+    error();
+  execve(args[0], args, env_vars);
+  error();  
+}
+
+void  redirect_in_out(int input_fd, int output_fd)
+{
+  if (input_fd == -1 || output_fd == -1)
+    error();
+  dup2(input_fd, STDIN_FILENO);
+  dup2(output_fd, STDOUT_FILENO);
+  close(input_fd);
+  close(output_fd);
+}
+
 void  error(void)
 {
   perror("Error");
